31aa4174927d4db752ab96cb8a5e93c76e8566a4
Roipoussiere <roipoussiere@protonmail.com>
2022-06-18 19:28:16 +0200

    configure build

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-class BodyPosture {
+export class BodyPosture {
 	scene: THREE.Scene
 	mesh: THREE.Mesh
 	renderer: THREE.WebGLRenderer
