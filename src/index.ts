143fc6784d62541799f3c049f0e0e384a80339b7
Roipoussiere <roipoussiere@protonmail.com>
2022-06-18 15:59:53 +0200

    extrenarlize css, add orbit control, rename to demo, non-fullscreen mode

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,38 @@
-// source: https://sbcode.net/threejs/add-initial-scripts/
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
+camera1.position.z = 2
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const canvas1 = document.getElementById('tc1') as HTMLCanvasElement
+
+const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
+renderer1.setSize(600, 450)
+
+new OrbitControls(camera1, renderer1.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
+	color: 0x00ff00,
+	wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
 function animate() {
-    requestAnimationFrame(animate)
+	requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+	cube.rotation.x += 0.01
+	cube.rotation.y += 0.01
 
-    render()
+	render()
 }
 
 function render() {
-    renderer.render(scene, camera)
+	renderer1.render(scene, camera1)
 }
 
 animate()
