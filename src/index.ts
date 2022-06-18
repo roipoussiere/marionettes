6150e78fcf1e7f4f4e4e2e016054c6cbc48ccb9d
Roipoussiere <roipoussiere@protonmail.com>
2022-06-18 17:41:01 +0200

    create Bodyposture class

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,49 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene = new THREE.Scene()
 
-const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
-camera1.position.z = 2
+class BodyPosture {
+	scene: THREE.Scene
+	mesh: THREE.Mesh
+	renderer: THREE.WebGLRenderer
+	camera: THREE.PerspectiveCamera
 
-const canvas1 = document.getElementById('tc1') as HTMLCanvasElement
+	constructor() {
+		this.scene = new THREE.Scene()
 
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
-renderer1.setSize(600, 450)
+		this.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
+		this.camera.position.z = 2
+		
+		const canvas1 = document.getElementById('tc1') as HTMLCanvasElement
+		
+		this.renderer = new THREE.WebGLRenderer({ canvas: canvas1 })
+		this.renderer.setSize(600, 450)
+		
+		new OrbitControls(this.camera, this.renderer.domElement)
+		
+		const geometry = new THREE.BoxGeometry()
+		const material = new THREE.MeshBasicMaterial({
+			color: 0x00ff00,
+			wireframe: true,
+		})
+		
+		this.mesh = new THREE.Mesh(geometry, material)
+		this.scene.add(this.mesh)
+	}
 
-new OrbitControls(camera1, renderer1.domElement)
+	animate(callback: FrameRequestCallback) {
+		requestAnimationFrame(callback)
+		this.mesh.rotation.x += 0.01
+		this.mesh.rotation.y += 0.01
+	
+		this.render()	
+	}
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-	color: 0x00ff00,
-	wireframe: true,
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-function animate() {
-	requestAnimationFrame(animate)
-
-	cube.rotation.x += 0.01
-	cube.rotation.y += 0.01
-
-	render()
+	render() {
+		this.renderer.render(this.scene, this.camera)
+	}
 }
 
-function render() {
-	renderer1.render(scene, camera1)
-}
-
+const body_posture = new BodyPosture()
+let animate = () => body_posture.animate(animate)
 animate()
