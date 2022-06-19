9279e542e33da3b4b36f049fb6fa13d73bd3215e
Roipoussiere <roipoussiere@protonmail.com>
2022-06-19 09:25:41 +0200

    refactor BodyPosture class

--- src/body_posture.ts
+++ src/body_posture.ts
@@ -6,35 +6,34 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 export class BodyPosture {
 	canvas: HTMLCanvasElement
 	scene: THREE.Scene
-	renderer: THREE.WebGLRenderer
 	camera: THREE.Camera
+	renderer: THREE.WebGLRenderer
 	control: OrbitControls
+	fbxLoader: FBXLoader
 
 	constructor(canvas_id: string) {
 		this.canvas = <HTMLCanvasElement> document.getElementById(canvas_id)
 		this.scene = new THREE.Scene()
-
 		this.camera = new THREE.PerspectiveCamera(75, 1, 0.5, 20)
+		this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas })
+		this.control = new OrbitControls(this.camera, this.renderer.domElement)
+		this.fbxLoader = new FBXLoader()
+	}
+
+	init() {
 		this.camera.position.y = 2
 		this.camera.position.z = 3
-	
-		this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas })
 		this.renderer.setSize(600, 450)
-		
-		this.control = new OrbitControls(this.camera, this.renderer.domElement)
 
-		const light = new THREE.PointLight()
-		light.position.set(0.8, 1.4, 1.0)
-		this.scene.add(light)
+		// this.scene.add(THREE.SkeletonHelper( skinnedMesh ));
+		this.addFbx('./xbot-light.fbx')
+		this.addGrid()
+		this.addLights()
+	}
 
-		const ambientLight = new THREE.AmbientLight()
-		this.scene.add(ambientLight)
-
-		this.scene.add(new THREE.GridHelper( 2, 10 ))
-
-		const fbxLoader = new FBXLoader()
-		fbxLoader.load(
-			'./xbot-light.fbx',
+	addFbx(model_path: string) {
+		this.fbxLoader.load(
+			model_path,
 			object => {
 				object.traverse(function (child) {
 					if (child.type == 'Bone') {
@@ -53,8 +52,21 @@ export class BodyPosture {
 			xhr => console.log((xhr.loaded / xhr.total) * 100 + '% loaded'),
 			error => console.log(error)
 		)
-		// this.scene.add(THREE.SkeletonHelper( skinnedMesh ));
+	}
 
+	addGrid() {
+		let grid = new THREE.GridHelper( 4, 20 )
+		grid.position.y = 0.01
+		this.scene.add(grid)
+	}
+
+	addLights() {
+		const light = new THREE.PointLight()
+		light.position.set(0.8, 1.4, 1.0)
+		this.scene.add(light)
+
+		const ambientLight = new THREE.AmbientLight()
+		this.scene.add(ambientLight)
 	}
 
 	animate(callback: FrameRequestCallback) {
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { BodyPosture } from './body_posture'
 
 
 const body_posture = new BodyPosture('three')
+body_posture.init()
 let animate = () => body_posture.animate(animate)
 animate()
