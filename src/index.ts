7716b757e3dc947f5c84d8067c6041ec1c9e18b7
Roipoussiere <roipoussiere@protonmail.com>
2022-06-19 12:31:48 +0200

    define scene size according to canvas dom size

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { BodyPosture } from './body_posture'
 
 
-const body_posture = new BodyPosture('three', [600, 450], './xbot-three.glb')
+const body_posture = new BodyPosture('three', './xbot-three.glb')
 // const body_posture = new BodyPosture('three', './xbot-light.fbx')
 body_posture.init()
 let animate = () => body_posture.animate(animate)
