a99dc7740f8b1253dfe509ce92203f81922cf260
Roipoussiere <roipoussiere@protonmail.com>
2022-07-31 01:07:56 +0200

    index.ts: add mat

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,11 @@ const base  = new Marionette('base' , params.get('_base' ) || DEFAULT_POSE_BASE)
 const flyer = new Marionette('flyer', params.get('_flyer') || DEFAULT_POSE_FLYER)
 const cam_pos = params.get('cam') || DEFAULT_CAM_POS
 
+const mat = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 0.03, 2),
+	new THREE.MeshPhongMaterial({ color: 0xc399c8 })
+)
+
 const model_loader = new ModelLoader(MODEL_PATH, model => {
 	model.scale.setScalar(0.01)
 
@@ -42,6 +47,7 @@ const theater = new Theater('cv1', [ base, flyer ], () => {
 	window.history.pushState({}, '', new_url)
 })
 theater.camera_pos = cam_pos
+theater.scene.add(mat)
 
 const main = new Main(model_loader, [ theater ])
 main.init()
